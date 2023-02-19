38b8aedd55ef8e01150ad4330d40b3a178c79fc7
Fraser Todd <flt515@york.ac.uk>
2023-02-19 20:09:13 +0000

    added Presentation.java and an updated Slide.java with appropriate tests

--- src/test/java/sweng/group/one/client_app_desktop/presentation/SlideTest.java
+++ src/test/java/sweng/group/one/client_app_desktop/presentation/SlideTest.java
@@ -3,8 +3,12 @@ package sweng.group.one.client_app_desktop.presentation;
 import static org.junit.Assert.*;
 
 import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Point;
 import java.util.ArrayList;
 import java.util.Arrays;
+
+import javax.swing.JFrame;
 
 //import org.assertj.core.util.Arrays;
 import org.junit.Test;
@@ -61,5 +65,58 @@ public class SlideTest {
 		}
 		
 	}
+	
+	@Test
+	public void preferredLayoutSizeTest() {
+		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(SLIDE_SIZE*3, SLIDE_SIZE*2);
+		frame.setVisible(true);
+		
+		Slide slide = new Slide(SLIDE_SIZE, SLIDE_SIZE);
+		frame.add(slide);
+		Dimension d = slide.preferredLayoutSize(frame);
+		
+		//test if dimensions get as large as frame height
+		assertEquals(true, d.height == frame.getHeight());
+		assertEquals(false, d.width == frame.getWidth());
+		
+		//change aspect ratio of the panel and test again for when the frame is taller than wide
+		frame.setSize(SLIDE_SIZE, SLIDE_SIZE*2);
+		d = slide.preferredLayoutSize(frame);
+		assertEquals(false, d.height == frame.getHeight());
+		assertEquals(true, d.width == frame.getWidth());
+		
+		//test if aspect ratio is 1:1
+		assertEquals(true, d.height == d.width);
+		frame.dispose();
+	}
 
+	@Test
+	public void layoutContainerTest() {
+		//create frame to add to
+		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(SLIDE_SIZE, SLIDE_SIZE);
+		frame.setVisible(true);
+		
+		Slide slide = new Slide(SLIDE_SIZE, SLIDE_SIZE);
+		slide.setPreferredSize(new Dimension(SLIDE_SIZE, SLIDE_SIZE));
+		
+		PresElement e = new DemoElement(new Point(0, 0), SLIDE_SIZE/2, SLIDE_SIZE/2, 0, slide);
+		slide.add(e);
+		
+		frame.add(slide);
+		frame.validate();
+		
+		slide.layoutContainer(frame);
+		
+		//component is where it is meant to be
+		assertEquals(e.component, slide.getComponentAt(1, 1));
+		
+		//component is not where it is not meant to be
+		assertEquals(slide, slide.getComponentAt(slide.getWidth()-1, slide.getHeight()-1));
+		frame.dispose();
+	}
+	
 }
