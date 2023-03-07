5db7d2e2e086c11dbf7471281765fcd4c80f811f
Giuseppe Barillari <joe2k01dev@gmail.com>
2023-03-07 12:00:00 +0100

    Add TextElement class
    Signed-off-by: Giuseppe Barillari <joe2k01dev@gmail.com>

---
+++ src/main/java/sweng/group/one/client_app_desktop/text/TextElement.java
@@ -0,0 +1,37 @@
+package sweng.group.one.client_app_desktop.text;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.GraphicsEnvironment;
+import java.awt.Point;
+import java.util.Arrays;
+
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+
+import sweng.group.one.client_app_desktop.presentation.PresElement;
+import sweng.group.one.client_app_desktop.presentation.Slide;
+
+public class TextElement extends PresElement {
+
+	public TextElement(String text, String fontName, int fontSizePt, Color Colour, float duration, Point pos, int width,
+			int height, Slide slide) {
+		super(pos, width, height, duration, slide);
+
+		Font[] t = GraphicsEnvironment.getLocalGraphicsEnvironment().getAllFonts();
+
+		Font loadedFont = Arrays.stream(t).filter(font -> font.getFontName().equals(fontName)).findFirst().orElse(null);
+
+		Font validFont = (loadedFont != null) ? new Font(loadedFont.getFontName(), Font.PLAIN, fontSizePt)
+				: new Font("Arial", Font.PLAIN, fontSizePt);
+
+		this.component = new JLabel(text);
+		this.component.setForeground(Colour);
+		this.component.setFont(validFont);
+	}
+	
+	public JComponent getComponent() {
+		return this.component;
+	}
+
+}
