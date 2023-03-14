67a4aeb9d6296bb91cd815d3471a9b0c93134dcd
Giuseppe Barillari <joe2k01dev@gmail.com>
2023-03-14 21:53:58 +0000

    Fix canCreateText test
    Signed-off-by: Giuseppe Barillari <joe2k01dev@gmail.com>

--- src/test/java/sweng/group/one/client_app_desktop/text/TextElementTest.java
+++ src/test/java/sweng/group/one/client_app_desktop/text/TextElementTest.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import java.awt.Color;
+import java.awt.Font;
+import java.awt.GraphicsEnvironment;
 import java.awt.Point;
 
 import javax.swing.JComponent;
@@ -19,14 +21,16 @@ public class TextElementTest {
 	private static final int SLIDE_Y = 400;
 	private static final int FONT_SIZE = 22;
 	private static final String CONTENT = "Test Content";
-	private static final String FONT = "Serif";
 
 	@Test
 	public void canCreateText() {
 		Slide slide = new Slide(SLIDE_X, SLIDE_Y);
 		
+		Font[] t = GraphicsEnvironment.getLocalGraphicsEnvironment().getAllFonts();
+		String fontName = t[0].getFontName();
+		
 		// Create element
-		TextElement classUnderTest = new TextElement(CONTENT, FONT, FONT_SIZE, Color.RED, 0,
+		TextElement classUnderTest = new TextElement(CONTENT, fontName, FONT_SIZE, Color.RED, 0,
 				new Point(SLIDE_X / 2, SLIDE_Y / 2), SLIDE_X / 4, SLIDE_Y / 4, slide);
 
 		JComponent jComponent = classUnderTest.getComponent();
@@ -35,7 +39,7 @@ public class TextElementTest {
 
 			// Check attributes
 			assertEquals("<html>" + CONTENT + "</html>", component.getText());
-			assertEquals(FONT, component.getFont().getFontName());
+			assertEquals(fontName, component.getFont().getFontName());
 			assertEquals(FONT_SIZE, component.getFont().getSize());
 		} else
 			fail();
